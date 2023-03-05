d6844636ab4e5eb687ed7d9c61dde1308ed51f82
Sudip Das <sudip5428@gmail.com>
2023-03-06 00:49:32 +0530

    initial commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { NavberComponent } from './navber/navber.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ShowEmployeeComponent } from './show-employee/show-employee.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    NavberComponent,
+    FooterComponent,
+    LoginComponent,
+    AddEmployeeComponent,
+    PageNotFoundComponent,
+    ShowEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
