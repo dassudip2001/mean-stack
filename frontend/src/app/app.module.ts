0321c803487f8a220bede4c9335ec89d5de67dd8
Sudip Das <sudip5428@gmail.com>
2023-03-07 00:50:08 +0530

    code commit for footer component add

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
-import { NavberComponent } from './navber/navber.component';
-import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
-import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-import {NgxPaginationModule} from 'ngx-pagination';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ShowEmployeeComponent } from './show-employee/show-employee.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import {MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { NgModule, isDevMode } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { EmployeeComponent } from "./employee/employee.component";
+import { NavberComponent } from "./navber/navber.component";
+import { FooterComponent } from "./footer/footer.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatIconModule } from "@angular/material/icon";
+import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from "./login/login.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { AddEmployeeComponent } from "./add-employee/add-employee.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { NgxPaginationModule } from "ngx-pagination";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ShowEmployeeComponent } from "./show-employee/show-employee.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { Ng2SearchPipeModule } from "ng2-search-filter";
+import { MatInputModule } from "@angular/material/input";
+import { MatNativeDateModule } from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import {MatInputModule} from '@angular/material/input';
     LoginComponent,
     AddEmployeeComponent,
     PageNotFoundComponent,
-    ShowEmployeeComponent
+    ShowEmployeeComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,14 +50,15 @@ import {MatInputModule} from '@angular/material/input';
     MatDatepickerModule,
     NgxPaginationModule,
     Ng2SearchPipeModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
+    MatNativeDateModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: "registerWhenStable:30000",
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
